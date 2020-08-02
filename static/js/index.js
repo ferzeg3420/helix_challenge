6a3c0ac26f2908b12088265a786661b87ca18ae6
Fernando <fzegadar@ucsc.edu>
2020-08-01 17:27:12 -0700

    Kind of finished. I now just want to move around a couple of files.

--- static/js/index.js
+++ static/js/index.js
@@ -3,14 +3,21 @@ let app = {};
 let init = (app) => {
     app.data = {
         sofa_color: "red",
+        is_mobile: false,
     };
 
     app.select = (sofa_color) => {
         app.vue.sofa_color = sofa_color;
     };
 
+    app.update_is_mobile = () => {
+        const width = window.innerWidth;
+        app.vue.is_mobile = width < 600;
+    };
+
     app.methods = {
         select: app.select,
+        update_is_mobile: app.update_is_mobile,
     };
 
     app.vue = new Vue({
@@ -20,10 +27,14 @@ let init = (app) => {
     });
 
     app.init = () => {
+        const width = window.innerWidth;
         app.vue.sofa_color = "red";
+        app.vue.is_mobile = width < 600;
     };
 
     app.init();
 };
 
 init(app);
+
+window.onresize = app.update_is_mobile;
